32f98e777374ccadc4cbf035993f13fa7ec048c0
tbnritzdoge <shadowphoenixcoc@gmail.com>
2021-06-18 23:13:28 -0400

    chore: multi-thread benchmark server

--- benchmarks/server.mjs
+++ benchmarks/server.mjs
@@ -1,20 +1,25 @@
+
+import cluster from "cluster";
+import { cpus } from "os";
 import { createServer } from "http";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { readFile } from "fs/promises";
 
 // @ts-expect-error: the file is running as a module, import.meta is allowed
 const largeData = (await readFile(`${dirname(fileURLToPath(import.meta.url))}/large.txt`)).toString();
+const workers = parseInt(process.env.WORKERS, 10) || cpus().length;
 
 function requestHandler(req, res) {
 	switch (req.url) {
 		case "/ok": {
-			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.writeHead(200, { "Content-Type": "text/plain" });
 			res.end("ok");
 			return;
 		}
 		case "/large": {
-			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.writeHead(200, { "Content-Type": "text/plain" });
 			res.end(largeData);
 			return;
 		}
@@ -24,5 +29,9 @@ function requestHandler(req, res) {
 	}
 }
 
-createServer(requestHandler)
-	.listen(8080, () => console.log("Awaiting requests."));
+// eslint-disable-next-line no-plusplus
+if (cluster.isPrimary) for (let ii = 0; ii < workers; ++ii) cluster.fork();
+else {
+	const server = createServer(requestHandler).listen(8080, () => console.log("Awaiting requests."));
+	server.keepAliveTimeout = 600e3;
+}
